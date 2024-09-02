3d86eeec555c300e05ff767a1bc767734c104381
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-09-02 15:46:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+// Returns the customerName
+var customerName = 'bob'
+
+// modifies the customerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Overwrites the best customer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Unsuccessfully tries to reassign the least favorite customer
+const leastFavoriteCustomer = 'Tom'
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Tom';
+}
